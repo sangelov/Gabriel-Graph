4044ca93c0a5d3dc0d23754164263b44b680d981
Svetlozar Angelov <angelov_sa@yahoo.com>
2012-02-01 19:25:00 +0200

    Refactor a little bit, create DelaunayTriangulation type that contains the relevant info

---
+++ Core/DelaunayTriangulation.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using DelaunayTriangulator;
+using S_hull;
+
+namespace Gabriel_Graph
+{
+	public class DelaunayTriangulation
+	{
+		private const int TriangleTickness = 1;
+		private const int TriangleZIndex = 2;
+		private static readonly Color triangleStrokeColor = Colors.Black;
+
+		private const int CircleZIndex = 1;
+		private const int CircleStrokeTickness = 3;
+		private static readonly Color circleStrokeColor = Colors.Black;
+
+		private List<Vertex> points;
+		private List<Triad> triads;
+
+		private Dictionary<Triad, Polygon> triadsDict;
+		private Dictionary<Polygon, Triad> triangles;
+		private Dictionary<Polygon, Path> circles;
+
+		private HashSet<Vertex> gabrielVertices;
+
+		private const double GabrielVertexRadius = 3;
+		private const double GabrielVertexTickness = 2;
+		private static readonly Color gabrielVertexColor = Colors.Red;
+
+		private DelaunayTriangulation()
+		{
+		}
+
+		public static DelaunayTriangulation Create()
+		{
+			DelaunayTriangulation delaunay = new DelaunayTriangulation();
+			delaunay.points = Input.Points;
+			delaunay.triangles = new Dictionary<Polygon, Triad>();
+			delaunay.circles = new Dictionary<Polygon, Path>();
+			delaunay.triadsDict = new Dictionary<Triad, Polygon>();
+			Triangulator angulator = new Triangulator();
+			delaunay.triads = angulator.Triangulation(delaunay.points, true);
+			return delaunay;
+		}
+
+		public IEnumerable<Polygon> GetDelaunayTriangulationPolygons()
+		{
+			foreach (var triad in this.triads)
+			{
+				if (!triadsDict.ContainsKey(triad))
+				{
+					Polygon polygon = CreateTriangle(triad);
+					this.triangles[polygon] = triad;
+					this.triadsDict[triad] = polygon;
+					yield return polygon;
+				}
+				else
+				{
+					yield return triadsDict[triad];
+				}
+
+			}
+		}
+
+		public Path CreateGabrielVertexPoint(Vertex vertex)
+		{
+			EllipseGeometry geometry = new EllipseGeometry();
+			geometry.Center = new Point(vertex.X, vertex.Y);
+			geometry.RadiusX = geometry.RadiusY = GabrielVertexRadius;
+			geometry.Freeze();
+			Path path = new Path();
+			path.StrokeThickness = GabrielVertexTickness;
+			path.Stroke = new SolidColorBrush(gabrielVertexColor);
+			path.Data = geometry;
+			return path;
+		}
+
+		public Path GetCircumCircle(Polygon polygon)
+		{
+			if (!circles.ContainsKey(polygon))
+			{
+				Triad triad = triangles[polygon];
+				EllipseGeometry geometry = new EllipseGeometry();
+				geometry.Center = new Point(triad.CircumcircleX, triad.CircumcircleY);
+				geometry.RadiusX = geometry.RadiusY = Math.Sqrt(triad.CircumcircleR2);
+				geometry.Freeze();
+				Path path = new Path();
+				Panel.SetZIndex(path, CircleZIndex);
+				path.StrokeThickness = CircleStrokeTickness;
+				path.Stroke = new SolidColorBrush(circleStrokeColor);
+				path.Data = geometry;
+				circles[polygon] = path;
+			}
+			return circles[polygon];
+		}
+
+		public IEnumerable<Vertex> GetGabrielVertices()
+		{
+			if (this.gabrielVertices == null)
+			{
+				this.gabrielVertices = new HashSet<Vertex>();
+				foreach (var triad in this.triads)
+				{
+					Triad neigbour;
+					if (triad.Ab > -1 && triad.Ab < triads.Count)
+					{
+						neigbour = this.triads[triad.Ab];
+						if (LinesIntersect(new Vertex(triad.CircumcircleX, triad.CircumcircleY),
+							new Vertex(neigbour.CircumcircleX, neigbour.CircumcircleY),
+							this.points[triad.A], this.points[triad.B]))
+						{
+							this.gabrielVertices.Add(this.points[triad.A]);
+							this.gabrielVertices.Add(this.points[triad.B]);
+						}
+					}
+					if (triad.Ac > -1 && triad.Ac < triads.Count)
+					{
+						neigbour = this.triads[triad.Ac];
+						if (LinesIntersect(new Vertex(triad.CircumcircleX, triad.CircumcircleY),
+							new Vertex(neigbour.CircumcircleX, neigbour.CircumcircleY),
+							this.points[triad.A], this.points[triad.C]))
+						{
+							{
+								this.gabrielVertices.Add(this.points[triad.A]);
+								this.gabrielVertices.Add(this.points[triad.C]);
+							}
+						}
+					}
+					if (triad.Bc > -1 && triad.Bc < triads.Count)
+					{
+						neigbour = this.triads[triad.Bc];
+						if (LinesIntersect(new Vertex(triad.CircumcircleX, triad.CircumcircleY),
+							new Vertex(neigbour.CircumcircleX, neigbour.CircumcircleY),
+							this.points[triad.B], this.points[triad.C]))
+						{
+							this.gabrielVertices.Add(this.points[triad.B]);
+							this.gabrielVertices.Add(this.points[triad.C]);
+						}
+					}
+				}
+			}
+			foreach (var vertex in this.gabrielVertices)
+			{
+				yield return vertex;
+			}
+		}
+
+		private bool LinesIntersect(Vertex l1P1, Vertex l1P2, Vertex l2P1, Vertex l2P2)
+		{
+			float q = (l1P1.Y - l2P1.Y) * (l2P2.X - l2P1.X) - (l1P1.X - l2P1.X) * (l2P2.Y - l2P1.Y);
+			float d = (l1P2.X - l1P1.X) * (l2P2.Y - l2P1.Y) - (l1P2.Y - l1P1.Y) * (l2P2.X - l2P1.X);
+
+			if (d == 0)
+			{
+				return false;
+			}
+
+			float r = q / d;
+
+			q = (l1P1.Y - l2P1.Y) * (l1P2.X - l1P1.X) - (l1P1.X - l2P1.X) * (l1P2.Y - l1P1.Y);
+			float s = q / d;
+
+			if (r < 0 || r > 1 || s < 0 || s > 1)
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		private Polygon CreateTriangle(Triad triangle)
+		{
+			Polygon polygon = new Polygon();
+			Vertex a = points[triangle.A];
+			Vertex b = points[triangle.B];
+			Vertex c = points[triangle.C];
+			polygon.Points = new PointCollection(new Point[] { new Point(a.X, a.Y), new Point(b.X, b.Y), new Point(c.X, c.Y) });
+			polygon.StrokeThickness = TriangleTickness;
+			polygon.Stroke = new SolidColorBrush(triangleStrokeColor);
+			Panel.SetZIndex(polygon, TriangleZIndex);
+			return polygon;
+		}
+	}
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
-using DelaunayTriangulator;
-using S_hull;
 using System.Windows.Controls.Primitives;
 
 namespace Gabriel_Graph
@@ -16,24 +12,7 @@ namespace Gabriel_Graph
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		private const int triangleTickness = 1;
-		private const int triangleZIndex = 2;
-		private static readonly Color triangleStrokeColor = Colors.Black;
-
-		private const int circleZIndex = 1;
-		private const int circleStrokeTickness = 3;
-		private static readonly Color circleStrokeColor = Colors.Black;
-
-		private const float gabrielVertexRadius = 2;
-		private const int gabrielVertexTickness = 3;
-		private static readonly Color gabrielVertexColor = Colors.Red;
-
-		private List<Vertex> points;
-		private List<Triad> triads;
-		private HashSet<Vertex> gabrielVertices;
-
-		private Dictionary<Polygon, Triad> triangles;
-		private Dictionary<Polygon, Path> circles;
+		private DelaunayTriangulation delaunay;
 
 		public MainWindow()
 		{
@@ -43,118 +22,33 @@ namespace Gabriel_Graph
 
 		private void GenerateDelaunayTriangulation()
 		{
-			//get random points
-			this.points = Input.Points;
-			Triangulator angulator = new Triangulator();
-			this.triads = angulator.Triangulation(points, true);
-			DrawDelaunayTriangulation(points, triads);
+			delaunay = DelaunayTriangulation.Create();	
+			DrawDelaunayTriangulation();
 		}
 
-		private void DrawDelaunayTriangulation(List<Vertex> points, List<Triad> triangles)
+		private void DrawDelaunayTriangulation()
 		{
 			this.Plane.Children.Clear();
-			this.triangles = new Dictionary<Polygon, Triad>();
-			this.circles = new Dictionary<Polygon, Path>();
-
-			foreach (var triangle in triangles)
+			
+			foreach (var polygon in this.delaunay.GetDelaunayTriangulationPolygons())
 			{
-				Polygon polygon = CreateTriangle(points, triangle);
-				this.triangles[polygon] = triangle;
 				Plane.Children.Add(polygon);
 			}
 		}
 
 		private void DrawGabrielGraphVertices()
 		{
-			foreach (var vertex in gabrielVertices)
+			foreach (var vertex in delaunay.GetGabrielVertices())
 			{
-				EllipseGeometry geometry = new EllipseGeometry();
-				geometry.Center = new Point(vertex.X, vertex.Y);
-				geometry.RadiusX = geometry.RadiusY = gabrielVertexRadius;
-				geometry.Freeze();
-				Path path = new Path();
-				path.StrokeThickness = gabrielVertexTickness;
-				path.Stroke = new SolidColorBrush(gabrielVertexColor);
-				path.Data = geometry;
+				Path path = delaunay.CreateGabrielVertexPoint(vertex);
 				this.Plane.Children.Add(path);
 			}
-		}
-
-		private void GetGabrielVertices()
-		{
-			this.gabrielVertices = new HashSet<Vertex>();
-			foreach (var triad in this.triads)
-			{
-				Triad neigbour;
-				if (triad.Ab > -1 && triad.Ab < triads.Count)
-				{
-					neigbour = this.triads[triad.Ab];
-					if (LinesIntersect(new Vertex(triad.CircumcircleX, triad.CircumcircleY),
-						new Vertex(neigbour.CircumcircleX, neigbour.CircumcircleY),
-						this.points[triad.A], this.points[triad.B]))
-					{
-						this.gabrielVertices.Add(this.points[triad.A]);
-						this.gabrielVertices.Add(this.points[triad.B]);
-					}
-				}
-				if (triad.Ac > -1 && triad.Ac < triads.Count)
-				{
-					neigbour = this.triads[triad.Ac];
-					if (LinesIntersect(new Vertex(triad.CircumcircleX, triad.CircumcircleY),
-						new Vertex(neigbour.CircumcircleX, neigbour.CircumcircleY),
-						this.points[triad.A], this.points[triad.C]))
-					{
-						{
-							this.gabrielVertices.Add(this.points[triad.A]);
-							this.gabrielVertices.Add(this.points[triad.C]);
-						}
-					}
-				}
-				if (triad.Bc > -1 && triad.Bc < triads.Count)
-				{
-					neigbour = this.triads[triad.Bc];
-					if (LinesIntersect(new Vertex(triad.CircumcircleX, triad.CircumcircleY),
-						new Vertex(neigbour.CircumcircleX, neigbour.CircumcircleY),
-						this.points[triad.B], this.points[triad.C]))
-					{
-						this.gabrielVertices.Add(this.points[triad.B]);
-						this.gabrielVertices.Add(this.points[triad.C]);
-					}
-				}
-			}
-		}
-
-		private Polygon CreateTriangle(List<Vertex> points, Triad triangle)
-		{
-			Polygon polygon = new Polygon();
-			Vertex a = points[triangle.A];
-			Vertex b = points[triangle.B];
-			Vertex c = points[triangle.C];
-			polygon.Points = new PointCollection(new Point[] { new Point(a.X, a.Y), new Point(b.X, b.Y), new Point(c.X, c.Y) });
-			polygon.StrokeThickness = triangleTickness;
-			polygon.Stroke = new SolidColorBrush(triangleStrokeColor);
-			Panel.SetZIndex(polygon, triangleZIndex);
-			return polygon;
-		}
-
-		private Path CreateCircle(Polygon polygon, Triad triad)
-		{
-			EllipseGeometry geometry = new EllipseGeometry();
-			geometry.Center = new Point(triad.CircumcircleX, triad.CircumcircleY);
-			geometry.RadiusX = geometry.RadiusY = Math.Sqrt(triad.CircumcircleR2);
-			geometry.Freeze();
-			Path path = new Path();
-			Panel.SetZIndex(path, circleZIndex);
-			path.StrokeThickness = circleStrokeTickness;
-			path.Stroke = new SolidColorBrush(circleStrokeColor);
-			path.Data = geometry;
-			return path;
 		}
 
 		private void FillTriangles()
 		{
 			Random rand = new Random();
-			foreach (var polygon in triangles.Keys)
+			foreach (var polygon in delaunay.GetDelaunayTriangulationPolygons())
 			{
 				polygon.Fill = new SolidColorBrush(Color.FromArgb(125, (byte)rand.Next(255), (byte)rand.Next(255), (byte)rand.Next(255)));
 				polygon.MouseEnter += OnHover;
@@ -164,7 +58,7 @@ namespace Gabriel_Graph
 
 		private void ClearTriangles()
 		{
-			foreach (var polygon in triangles.Keys)
+			foreach (var polygon in this.delaunay.GetDelaunayTriangulationPolygons())
 			{
 				polygon.Fill = new SolidColorBrush(Colors.White);
 				polygon.MouseEnter -= OnHover;
@@ -177,7 +71,8 @@ namespace Gabriel_Graph
 			Polygon polygon = sender as Polygon;
 			if (polygon != null)
 			{
-				this.Plane.Children.Remove(circles[polygon]);
+				Path path = delaunay.GetCircumCircle(polygon);
+				this.Plane.Children.Remove(path);
 			}
 		}
 
@@ -186,27 +81,15 @@ namespace Gabriel_Graph
 			Polygon polygon = sender as Polygon;
 			if (polygon != null)
 			{
-				if (circles.ContainsKey(polygon) && !this.Plane.Children.Contains(circles[polygon]))
-				{
-					this.Plane.Children.Add(circles[polygon]);
-				}
-				else
-				{
-					Triad triad = this.triangles[polygon];
-					if (triad != null)
-					{
-						Path path = CreateCircle(polygon, triad);
-						this.circles[polygon] = path;
-						this.Plane.Children.Add(path);
-					}
-				}
+				Path path = delaunay.GetCircumCircle(polygon);
+				this.Plane.Children.Add(path);
 			}
 		}
 
 		private void GenerateClick(object sender, RoutedEventArgs e)
 		{
 			GenerateDelaunayTriangulation();
-			if (ColorTrianglesButton.IsChecked.HasValue && (bool)ColorTrianglesButton.IsChecked.Value)
+			if (ColorTrianglesButton.IsChecked.HasValue && ColorTrianglesButton.IsChecked.Value)
 			{
 				FillTriangles();
 			}
@@ -217,7 +100,7 @@ namespace Gabriel_Graph
 			ToggleButton btn = sender as ToggleButton;
 			if (btn != null)
 			{
-				if (btn.IsChecked.HasValue && (bool)btn.IsChecked.Value)
+				if (btn.IsChecked.HasValue && btn.IsChecked.Value)
 				{
 					FillTriangles();
 				}
@@ -230,31 +113,7 @@ namespace Gabriel_Graph
 
 		private void BuildGabrielGraphClick(object sender, RoutedEventArgs e)
 		{
-			GetGabrielVertices();
 			DrawGabrielGraphVertices();
-		}
-
-		private bool LinesIntersect(Vertex l1p1, Vertex l1p2, Vertex l2p1, Vertex l2p2)
-		{
-			float q = (l1p1.Y - l2p1.Y) * (l2p2.X - l2p1.X) - (l1p1.X - l2p1.X) * (l2p2.Y - l2p1.Y);
-			float d = (l1p2.X - l1p1.X) * (l2p2.Y - l2p1.Y) - (l1p2.Y - l1p1.Y) * (l2p2.X - l2p1.X);
-
-			if (d == 0)
-			{
-				return false;
-			}
-
-			float r = q / d;
-
-			q = (l1p1.Y - l2p1.Y) * (l1p2.X - l1p1.X) - (l1p1.X - l2p1.X) * (l1p2.Y - l1p1.Y);
-			float s = q / d;
-
-			if (r < 0 || r > 1 || s < 0 || s > 1)
-			{
-				return false;
-			}
-
-			return true;
 		}
 	}
 }
